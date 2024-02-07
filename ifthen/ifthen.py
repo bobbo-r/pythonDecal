55addfee04ed76010d7db17f620e86700c9a70ec
bobbo-r <157636237+bobbo-r@users.noreply.github.com>
2024-02-07 10:39:19 -0800

    commit -m "beans"

---
+++ ifthen/ifthen.py
@@ -0,0 +1,23 @@
+
+#x=10
+#
+#if (x>10):
+#    print("x is greater than 10")
+
+#elif(x<10):
+  #  print("x is less than 10")
+
+#else:
+ #   print("x=10")
+
+#i=100
+#while i>0:
+   # print(i)
+    #i -= 1
+
+#for a in range(-1, 11):
+#    print(a)
+
+
+
+
